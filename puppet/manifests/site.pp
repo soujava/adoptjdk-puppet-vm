8f339ca0ecd3443c0d4a2eb180f97b83c27a368d
Thomas Modeneis <thomas.modeneis@gmail.com>
2015-05-01 22:07:45 +0200

    cleanup

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -8,24 +8,8 @@ class { 'baseconfig':
   stage => 'pre'
 }
 
-
 #install jdk
 class { 'java': }
-
-
-#prepare for build, get the submodules of jdk
-#exec { 'prepare build for openjdk8':
-#  path     => '/',
-#  command => 'chmod +x jdk8/get_source.sh && bash jdk8/get_source.sh';
-#}
-
-#compile and make images
-#exec { 'build openjdk8':
-#  path     => '/bin/bash',
-#  command => '/bin/cd ~/jdk8 && /bin/bash make images';
-#}
-
-
 
 # set defaults for file ownership/permissions
 File {
